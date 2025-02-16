02263110a909d201cd80537f424e321adc286e57
zeinirfansyah <12.zeinirfansyah@gmail.com>
2025-02-16 09:46:26 +0700

    feat [user_controllers] : Added "/me" endpoint to fetch user information

---
+++ src/controllers/user_controller/me.controller.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from 'express';
+
+export const me = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const user = req.user;
+
+    res.status(200).json({
+      success: true,
+      status: 200,
+      message: 'User fetched successfully',
+      data: user
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error('Error fetching user:', error.message);
+      res.status(500).json({
+        success: false,
+        status: 500,
+        message: error.message
+      });
+    } else {
+      console.error('Unexpected error:', error);
+      res.status(500).json({
+        success: false,
+        status: 500,
+        message: 'An unexpected error occurred'
+      });
+    }
+  }
+};
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 import { validateUserInput } from '../middlewares/validators/user.validation';
 import { createUser } from '../controllers/user_controller/create.controller';
+import { verifyToken } from '../middlewares/authorization';
+import { me } from '../controllers/user_controller/me.controller';
 
 const userRouter = Router();
 
+userRouter.get('/me', verifyToken, me);
 userRouter.post('/user', validateUserInput, createUser);
 
 export default userRouter;
