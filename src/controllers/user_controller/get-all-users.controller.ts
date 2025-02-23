9e3e1790010cfc7fa5055e48445d6fb3c8eb8cb6
zeinirfansyah <12.zeinirfansyah@gmail.com>
2025-02-23 20:48:24 +0700

    feat: Add role-based access control to getAllUsers endpoint

--- src/controllers/user_controller/get-all-users.controller.ts
+++ src/controllers/user_controller/get-all-users.controller.ts
@@ -5,14 +5,41 @@ export const getAllUsers = async (
   req: Request,
   res: Response
 ): Promise<void> => {
+  // req.user
+
+  const user = req.user;
+
+  if (!user) {
+    res.status(401).json({
+      message: 'Must be logged in',
+      data: null
+    });
+    return;
+  }
+
+  if (user.role !== 'ADMIN' && user.role !== 'SUPER_ADMIN') {
+    res.status(403).json({
+      message: 'You do not have the required role to access this resource',
+      data: null
+    });
+    return;
+  }
+
   try {
     const users = await getAllUsersService();
+    let filteredUsers;
+
+    if (user.role === 'ADMIN') {
+      filteredUsers = users?.filter((user) => user.role === 'USER_ACCOUNT');
+    } else if (user.role === 'SUPER_ADMIN') {
+      filteredUsers = users;
+    }
 
     res.status(201).json({
       success: true,
       status: 200,
       message: 'User fetched successfully',
-      data: users
+      data: filteredUsers
     });
   } catch (error) {
     if (error instanceof Error) {
