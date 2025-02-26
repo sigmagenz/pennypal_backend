657c2b53fa33b05bd7a134f1b45a53ef39026347
zeinirfansyah <12.zeinirfansyah@gmail.com>
2025-02-26 20:12:37 +0700

    refactor [me-router, user-router]: changed me-router structure and added auth to create-user route

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -3,13 +3,17 @@ import { validateUserInput } from '../middlewares/validators/user.validation';
 import { createUser } from '../controllers/user_controller/create-user.controller';
 import { authorizeRoles, verifyToken } from '../middlewares/authorization';
 import { getAllUsers } from '../controllers/user_controller/get-all-users.controller';
-import { getMe } from '../controllers/me/get-me.controller';
 import { getUser } from '../controllers/user_controller/get-user.controller';
 
 const userRouter = Router();
 
-userRouter.get('/me', verifyToken, getMe);
-userRouter.post('/user', validateUserInput, createUser);
+userRouter.post(
+  '/user',
+  verifyToken,
+  authorizeRoles(['ADMIN', 'SUPER_ADMIN']),
+  validateUserInput,
+  createUser
+);
 userRouter.get(
   '/user',
   verifyToken,
